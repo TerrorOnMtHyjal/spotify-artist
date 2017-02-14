7803959ab723fc3ebb8c0b3f8000b7851ed13bfc
dustwise <jared.mohney@gmail.com>
2017-02-14 13:44:51 -0700

    working on second call

--- spotify.js
+++ spotify.js
@@ -23,3 +23,25 @@ const getArtist = name => {
       return artist;  
     }).catch(error => {console.log("WHAT THE HELL IS THIS", error)});
 };
+
+
+
+// let artist;
+// const getArtist = name => {
+//     const query = {
+//         q: "Celine Dion",
+//         limit: 1,
+//         type: "artist"
+//     }
+
+//     return getFromAPI("search", query).then(result =>{
+//       artist = result.artists.items[0];
+//       //https://api.spotify.com/v1/artists/{id}/related-artists
+//       return fetch(`https://api.spotify.com/v1/artists/${artist.id}/related-artists`).then(relatedResults => {
+//           const parsed = relatedResults.json();
+//           artist.related = parsed.artists;
+//           console.log(parsed);
+//           return artist;
+//       });  
+//     }).catch(error => {console.log("WHAT THE HELL IS THIS", error)});
+// };
