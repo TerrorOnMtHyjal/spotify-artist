86cc7bdd86e3611b8badb589aad8c8a20e0b23dc
dustwise <jared.mohney@gmail.com>
2017-02-14 15:22:14 -0700

    done

--- spotify.js
+++ spotify.js
@@ -1,4 +1,4 @@
-const getFromAPI = function(endpoint, query) {
+const getFromAPI = function(endpoint, query = {}) {
     const url = new URL(`https://api.spotify.com/v1/${endpoint}`);
     Object.keys(query).forEach(key => url.searchParams.append(key, query[key]));
     return fetch(url).then(response => {
@@ -9,19 +9,35 @@ const getFromAPI = function(endpoint, query) {
     });
 };
 
-
 let artist;
 const getArtist = name => {
     const query = {
-        q: "Celine Dion",
+        q: "System of a Down",
         limit: 1,
         type: "artist"
     }
 
     return getFromAPI("search", query).then(result =>{
       artist = result.artists.items[0];
-      return artist;  
-    }).catch(error => {console.log("WHAT THE HELL IS THIS", error)});
+      return getFromAPI(`artists/${artist.id}/related-artists`);
+    }).then(relatedResults => {
+        const relatedArtists  = relatedResults.artists;
+        let artistTrackRequests = [];
+
+        artist.related = relatedArtists;
+
+        relatedArtists.forEach(artist => {
+            artistTrackRequests.push(getFromAPI(`artists/${artist.id}/top-tracks`, {country: "US"}));
+        });
+
+        return Promise.all(artistTrackRequests);
+    }).then(topTracks => {
+        topTracks.forEach((currentTracks, num) => {
+            artist.related[num].tracks = currentTracks.tracks;
+        });
+
+        return artist;
+    }).catch(error => {console.log(error)});
 };
 
 
